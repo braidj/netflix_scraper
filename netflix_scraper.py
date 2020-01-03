8526be717223023907173e57ee8081f57ed7d8ba
Jason Braid <braidj@gmail.com>
2020-01-03 16:07:12 +0000

    Added stats processing on Films and Shows (basic)

--- netflix_scraper.py
+++ netflix_scraper.py
@@ -4,8 +4,32 @@ import datetime
 import csv
 import collections
 
+films_dict = {}
+shows_dict = {}
+shows_cnt = collections.Counter()
+shows_list = []
+watched_dict = {}
+watched_list = []
 
-def parse_show(show,watched):
+
+def process_results(prog_list):
+    # -----------------------------------------------------------------------------------------------
+    # Created: 20200103 by Jason Braid
+    # Strip out season, episode name if applicable
+    # -----------------------------------------------------------------------------------------------
+
+    for item in prog_list:
+
+        if item.type == 'Film':
+            films_dict[item.title] = item.watched
+
+        if item.type == 'Show':
+            shows_list.append(item.title)
+
+        watched_list.append(item.watched)
+
+
+def parse_show(show, watched):
     # -----------------------------------------------------------------------------------------------
     # Created: 20200103 by Jason Braid
     # Strip out season, episode name if applicable
@@ -20,17 +44,21 @@ def parse_show(show,watched):
     episode = 'n/a'
 
     if show.count(':') == 0:
-        print('Film: %s' % show)
+        #print('Film: %s' % show)
         type = 'Film'
+        title = show
     elif show.count(':') == 2:
         title, season, episode = show.split(':')
-        print(f'SERIES: {title} - {season} - {episode}')
+        type = 'Show'
+       # print(f'SERIES: {title} - {season} - {episode}')
     elif show.count(':') == 1:
-        print('Film series ?: %s' % show)
+        #print('Film series ?: %s' % show)
         title, episode = show.split(':')
+        type = 'Film Series'
     else:
-        print('Investigate: %s' % show)
+        #print('Investigate: %s' % show)
         title = show
+        type = 'Other'
 
     return NetflixShow(type, title, season, episode, watched)
 
@@ -51,15 +79,32 @@ if __name__ == "__main__":
         for row in csv_reader:
 
             if line_count == 0:
-                print(f'Columns names are {", ".join(row)}')
+                #print(f'Columns names are {", ".join(row)}')
                 line_count += 1
             else:
                 show, day = row
-                results.append(parse_show(show,day))
+                results.append(parse_show(show, day))
 
                 line_count += 1
 
-    print(f'processed {len(results)} items.')
-    print(datetime.datetime.now())
+    print(f'processed {len(results)} items.\n')
+    process_results(results)
+
+    print(f"You have watched {len(films_dict)} films")
+    print(f"You have watched {len(shows_list)} shows")
+
+    cnt = collections.Counter(shows_list)
+
+    print("\nYour ten most popular shows are")
+    for k, v in cnt.most_common(10):
+        print(k, v)
+
+    cnt = collections.Counter(watched_list)
+
+    print("\nYour ten most viewed days were")
+    for k, v in cnt.most_common(10):
+        print(k, v)
+
+    #print(datetime.datetime.now())
 
     sys.exit(0)
